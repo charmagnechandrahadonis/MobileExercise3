df9dbf69afca08a455ee52de63d1c5480b93a72e
Charmagne Adonis <charmagne.g.adonis@gmail.com>
2020-10-20 09:51:30 +0800

    Initial Commit

---
+++ MobileExercise1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MobileExercise1
+//
+//  Created by Charmagne Adonis on 10/20/20.
+//  Copyright © 2020 Charmagne Adonis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
